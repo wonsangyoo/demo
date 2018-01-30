1a6f32e8e26aa3f58473054704f4ee527693268e
akarws <akarws@gmail.com>
2018-01-30 17:53:10 -0500

    more changes to index

--- src/GithubTest.java
+++ src/GithubTest.java
@@ -2,5 +2,6 @@
 public class GithubTest {
 	public static void main(String[] args) {
 		System.out.println("Hello, this is a Test of Github.");
+		System.out.println("More code to check changes...");
 	}
 }
